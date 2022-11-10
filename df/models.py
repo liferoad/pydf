eb08c4f7299f6294b97ede71ed85cbbff4909877
xqhu <xqhu@google.com>
2022-11-09 22:38:47 -0500

    working on data pipelines

--- df/models.py
+++ df/models.py
@@ -6,3 +6,12 @@ class Job(BaseModel):
     """Dataflow job information"""
 
     name: str = Field(..., description="Dataflow job name")
+
+
+class DataPipeline(BaseModel):
+    """Data Pipeline information"""
+
+    _api_results: str
+
+    class Config:
+        underscore_attrs_are_private = True
