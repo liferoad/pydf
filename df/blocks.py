922a768dc8928b9437be1593b6a774b9ec549e9e
xqhu <xqhu@google.com>
2023-01-01 15:55:21 -0500

    sim block

--- df/blocks.py
+++ df/blocks.py
@@ -12,6 +12,7 @@ from apache_beam.runners.direct import DirectRunner
 from apache_beam.runners.interactive.interactive_runner import InteractiveRunner
 from pydantic import BaseModel, Field, root_validator
 from sentence_transformers import SentenceTransformer
+from sentence_transformers import util as st_util
 
 
 def create_beam_pipeline() -> beam.Pipeline:
@@ -37,7 +38,7 @@ class Block(BaseModel):
     block_id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, description="block UUID4")
     source_ids: List[uuid.UUID] = []
     target_ids: List[uuid.UUID] = []
-    operation: Union[beam.ParDo, beam.Create] = Field(..., description="the block operation")
+    operation: Optional[Union[beam.ParDo, beam.Create]] = Field(None, description="the block operation")
     o: beam.pvalue.PCollection = Field(None, description="the output PCollection for the above operation")
     _sources: List[Block] = []
     _targets: List[Block] = []
@@ -51,8 +52,10 @@ class Block(BaseModel):
             sources = [sources]
         self._sources = sources
         for block in sources:
-            self.source_ids.append(block.block_id)
-            block.target_ids.append(self.block_id)
+            if block.block_id not in self.source_ids:
+                self.source_ids.append(block.block_id)
+            if block.block_id not in self.target_ids:
+                block.target_ids.append(self.block_id)
             block._targets.append(self)
         return self
 
@@ -85,6 +88,30 @@ class SentenceEmbeddingBlock(Block):
             lambda x: [SentenceTransformer(values.get("model_name", "all-MiniLM-L6-v2")).encode(x)]
         )
         return values
+
+
+def _cross_join(left, rights):
+    for x in rights:
+        yield (left, x)
+
+
+class CrossJoinBlock(Block):
+    """on the fly cross join block"""
+
+    block_type: str = "CrossJoin"
+
+    def __call__(self, sources: List[Block] = None) -> Block:
+        super().__call__(sources)
+        if sources[0].o and sources[1].o:
+            self.o = sources[0].o | f"{self.block_type} - {self.block_id}" >> beam.FlatMap(
+                _cross_join, rights=beam.pvalue.AsIter(sources[1].o)
+            )
+        return self
+
+
+class CosSimilarityBlock(Block):
+    block_type: str = "CosSimilarity"
+    operation: beam.ParDo = beam.Map(lambda x: float(st_util.cos_sim(x[0], x[1])[0][0]))
 
 
 class BlockAssembler:
@@ -128,11 +155,15 @@ class BlockAssembler:
         def _build_o(o, blocks, parsed_block):
             for block in blocks:
                 if block.block_id not in parsed_block:
-                    block.o = o | f"{block.block_type} - {block.block_id}" >> block.operation
+                    if block.operation:
+                        block.o = o | f"{block.block_type} - {block.block_id}" >> block.operation
+                    else:
+                        block(block._sources)
+                if block.o:
                     parsed_block.append(block.block_id)
-                if block.target_ids:
-                    targets = [self.id_to_block[t] for t in block.target_ids]
-                    _build_o(block.o, targets, parsed_block)
+                    if block.target_ids:
+                        targets = [self.id_to_block[t] for t in block.target_ids]
+                        _build_o(block.o, targets, parsed_block)
             return
 
         _build_o(self.p, self.blocks, [])
