5d20863e1ee99f02bf9ab92f322a873c82785585
xqhu <xqhu@google.com>
2023-01-15 15:01:45 -0500

    updated reqs

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # third party libraries
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name="df",
     version="0.0.1",
     description="A useful Dataflow module",
     author="dataflow",
     packages=["df"],  # same as name
-    install_requires=["wheel"],  # external packages as dependencies
+    install_requires=["wheel"] + required_packages,  # external packages as dependencies
 )
